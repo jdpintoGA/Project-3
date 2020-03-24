f035c91d5853303e2af1f17d33010b698aa4b818
Marius Zerni <marius_zerni@yahoo.com>
2020-03-24 17:31:39 +0000

    home page

---
+++ frontend/src/app.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import '../styles/style.scss'
+
+import Home from '../components/Home'
+// import NavBar from './components/NavBar'
+
+
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
